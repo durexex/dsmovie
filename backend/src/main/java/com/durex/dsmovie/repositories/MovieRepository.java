aec3674ecdab0c339e30feae90fda6d0397917fb
durexex <eduardodurieuxlopes@gmail.com>
2022-03-09 15:36:18 -0300

    Find movies

---
+++ backend/src/main/java/com/durex/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.durex.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.durex.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
